d8e02c6a816c0e60de67b4540a59e67c380cd25a
dthomas94 <danielle.thomas313@gmail.com>
2023-07-31 14:08:20 -0400

    adds route for user creation and retrieval

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: {status: 'SUCCESS', message: 'Loaded users', data: users}, status: :ok
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: {status: 'SUCCESS', message: 'User created', data: user}, status: :created
+        else
+            render json: {status: 'ERROR', message: 'User creation failed', data: user.errors}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :email, :password)
+    end
+end
