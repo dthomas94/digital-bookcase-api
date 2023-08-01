881247764c721669d465d5d43a0e337ba27967c3
dthomas94 <danielle.thomas313@gmail.com>
2023-08-01 15:22:00 -0400

    adds graphql support

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # USERS
-  get 'users', to: 'users#index'
-  post 'users/new', to: 'users#create'
-
-  # SESSIONS
-  post '/login', to: 'sessions#login'
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
 end
