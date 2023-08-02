153f9262a3d5d381e3df701b9926ddad66438b11
dthomas94 <danielle.thomas313@gmail.com>
2023-08-02 16:49:13 -0400

    validates user's password

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,11 +4,19 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
   private
 
+    
   def respond_with(current_user, _opts = {})
-    render json: {
-      status: { code: 200, message: 'Logged in successfully.'},
-      user: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
-    }, status: :ok
+    if current_user.valid_password?(params[:password])
+      render json: {
+        status: { code: 200, message: 'Logged in successfully.'},
+        user: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find user."
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # https://github.com/waiting-for-dev/devise-jwt
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
