0bae7f27ee6151e4ef8311b1db5f22d846cbe3e4
dthomas94 <danielle.thomas313@gmail.com>
2023-08-12 14:32:20 -0400

    updates workdata schema

--- app/graphql/types/work_data_type.rb
+++ app/graphql/types/work_data_type.rb
@@ -1,12 +1,12 @@
 module Types
     class WorkDataType < Types::BaseObject
-        field :key, String, description: "openlibrary ref string /authors/OL3964940A"
-        field :title, String
-        field :revision, Integer
-        field :latest_revision, Integer
-        field :last_modified, String, description: "datetime 2009-12-11T01:57:19.964652"
-        field :created, String, description: "datetime 2009-12-11T01:57:19.964652"
-        field :authors, [String], description: "array of openlibrary ref strings ['/authors/OL3964940A',...]"
-        field :covers, [Int]
+        field :key, String, null: false, description: "openlibrary ref string /authors/OL3964940A"
+        field :title, String, null: false
+        field :revision, Integer, null: false
+        field :latest_revision, Integer, null: false
+        field :last_modified, String, null: false, description: "datetime 2009-12-11T01:57:19.964652"
+        field :created, String, null: false, description: "datetime 2009-12-11T01:57:19.964652"
+        field :authors, [String], null: false, description: "array of openlibrary ref strings ['/authors/OL3964940A',...]"
+        field :covers, [Int], description: "array of cover identifiers for openlibrary"
     end
 end
