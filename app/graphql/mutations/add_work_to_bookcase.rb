ee59654cdb1c2dcc7de2fd1915198759cec6a561
dthomas94 <danielle.thomas313@gmail.com>
2023-08-20 16:37:06 -0400

    uses user stored in context for resolving mutations

--- app/graphql/mutations/add_work_to_bookcase.rb
+++ app/graphql/mutations/add_work_to_bookcase.rb
@@ -1,12 +1,11 @@
 module Mutations
   class AddWorkToBookcase < BaseMutation
     argument :work_key, String, required: true
-    argument :user_id, Integer, required: true
     
     field :bookcase, Types::BookcaseType, null: false
 
     def resolve(input)
-      bookcase = Bookcase.find_by(user_id: input[:user_id])
+      bookcase = Bookcase.find_by(user_id: context[:current_resource][:id])
 
       if bookcase && !bookcase.work_keys.include?(input[:work_key])
         bookcase.update!(work_keys: bookcase.work_keys.push(input[:work_key]))
--- app/graphql/mutations/remove_work_from_bookcase.rb
+++ app/graphql/mutations/remove_work_from_bookcase.rb
@@ -1,12 +1,11 @@
 module Mutations
     class RemoveWorkFromBookcase < BaseMutation
       argument :work_key, String, required: true
-      argument :user_id, Integer, required: true 
 
       field :bookcase, Types::BookcaseType, null: false
   
       def resolve(input)
-        bookcase = Bookcase.find_by(user_id: input[:user_id])
+        bookcase = Bookcase.find_by(user_id: context[:current_resource][:id])
         
         if bookcase && bookcase.work_keys.include?(input[:work_key])
             work_keys = bookcase.work_keys[0 .. bookcase.work_keys.length]
